9a5e7796271c04436c119cef18c4ddf0adfa70f7
12Lorenzo <lorenzocbm@gmail.com>
2021-10-22 22:47:51 -0300

    Aceitando e recusando propostas pt1.
    Co-authored-by: OakBranches <matheusramosimperador@gmail.com>

--- src/main/java/br/ufscar/dc/dsw1/controller/CarroController.java
+++ src/main/java/br/ufscar/dc/dsw1/controller/CarroController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/carro")
 public class CarroController {

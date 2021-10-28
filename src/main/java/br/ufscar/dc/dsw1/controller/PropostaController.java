4c30f3b85ccd71fd857005f98faf3e2df25d0220
12Lorenzo <lorenzocbm@gmail.com>
2021-10-28 16:11:43 -0300

    Fazendo a tradução para esperanto.
    Co-authored-by: OakBranches <matheusramosimperador@gmail.com>

--- src/main/java/br/ufscar/dc/dsw1/controller/PropostaController.java
+++ src/main/java/br/ufscar/dc/dsw1/controller/PropostaController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.ui.Model;
 
-import javax.jms.MessageProducer;
+//import javax.jms.MessageProducer;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
